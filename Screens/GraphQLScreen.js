18d8e0059c14fc02b93669801566e165479b2a54
Patrick Soethe <dont@know.me>
2020-09-07 22:18:11 +0200

    Tab Navigation für Rest und GraphQL

---
+++ Screens/GraphQLScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function GraphQLScreen() {
+
+    return (
+        <View style={{ flex: 1, padding: 24, marginTop: 30 }}>
+            <Text>GraphQL Screen</Text>
+        </View>
+    );
+}
---
+++ Screens/RESTScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function RESTScreen() {
+
+    return (
+        <View style={{ flex: 1, padding: 24, marginTop: 30 }}>
+            <Text>Rest Screen</Text>
+        </View>
+    );
+}
